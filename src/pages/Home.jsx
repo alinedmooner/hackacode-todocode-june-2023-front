8e1c990d82483bf97d2c182a8c919e0519d86dd2
Daniel M.C <daamorenoc@udistrital.edu.co>
2023-06-08 13:11:30 -0500

    🩹 fix: home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
+import Logo from '../../public/logo.svg'
+
 function Home() {
   return (
     <>
       <div className="main-container">
+        <img className="logo" src={Logo} alt="company logo" />
         <p className="main-text">Welcome to Dashboard of</p>
         <h1 className="main-title">Infinity Dream Park</h1>
         <p className="secondary-text">Login depending on your rank</p>
