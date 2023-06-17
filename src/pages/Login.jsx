dff6c12bac01c80f623bb9a999d62862a1c451fa
Daniel M.C <daamorenoc@udistrital.edu.co>
2023-06-17 00:33:23 -0500

    🩹 fix: login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import Logo from '../assets/logo.svg';
 
 function Login() {
   return (
-    <div className="form-container">
-      <img className="logo" src={Logo} alt="company logo" />
-      <h1 className="main-title">Infinity Dream Park</h1>
-      <h2 className="main-text">Welcome back</h2>
-      <form className="main-container" action="" method="">
+    <div>
+      <img src="" alt="Logo" />
+      <h1>Infinity Dream Park</h1>
+      <h2>Welcome back</h2>
+      <form action="" method="">
         <label htmlFor="user-name">
           Username
           <input type="text" id="user-name" placeholder="johndoe" />
@@ -16,9 +15,9 @@ function Login() {
           Password
           <input type="password" id="password" placeholder="********" />
         </label>
-        <button className="" type="submit">Sign In</button>
+        <button type="submit">Sign In</button>
       </form>
-      <a className="forgot-password" href="a">Forgot password?</a>
+      <a href="a">Forgot password?</a>
     </div>
   );
 }
