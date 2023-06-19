63da205dc348549c01b48466b6a662d77b64a355
Daniel M.C <daamorenoc@udistrital.edu.co>
2023-06-19 00:24:15 -0500

    ✨ feat: api slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,13 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { configureStore } from '@reduxjs/toolkit';
+import apiSlice from './slices/apiSlice';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
 });
 
 export default store;
