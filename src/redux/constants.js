5358b068b8246f1e78c2bc2392755a5f0f4c2a97
Daniel M.C <daamorenoc@udistrital.edu.co>
2023-06-19 00:31:41 -0500

    🩹 fix: api slice

---
+++ src/redux/constants.js
@@ -0,0 +1,3 @@
+export const BASE_URL = 'http://localhost:5273';
+export const CUSTOMERS_URL = '/api/customers';
+export const EMPLOYEES_URL = '/api/employees';
---
+++ src/redux/slices/apiSlice.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+import { BASE_URL } from '../constants';
+
+const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
+
+const apiSlice = createApi({
+  baseQuery,
+  tagTypes: ['Customers', 'Employees'],
+  endpoints: (builder) => ({}),
+});
+
+export default apiSlice;
