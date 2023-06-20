7abc4ac41e1b985c338f163173ce0a245cf3144a
Daniel M.C <daamorenoc@udistrital.edu.co>
2023-06-20 13:17:40 -0500

    🩹 fix: home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 function Home() {
   return (
-    <div >
+    <div>
       <img src="" alt="company logo" />
-      <p >Welcome to</p>
-      <h1 >Infinity Dream Park</h1>
-      <p >An Amusement and Adventure Park</p>
-      <a  href="/login">Login</a>
+      <p>Welcome to</p>
+      <h1>Infinity Dream Park</h1>
+      <p>An Amusement and Adventure Park</p>
+      <a href="/login">Login</a>
     </div>
   );
 }
