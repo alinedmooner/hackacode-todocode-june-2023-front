bafb7509e87cc17d6478ec1d8874950a14cff504
Daniel M.C <alinedmooner@gmail.com>
2023-07-02 18:26:40 -0500

    ✨ feat: test components

---
+++ src/pages/Test.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import EmployeeNavigation from '../components/EmployeeNavigation'
+import EmployeeHeader from '../components/EmployeeHeader'
+
+function Test () {
+  return <div>
+    <EmployeeHeader />
+    <EmployeeNavigation />
+  </div>
+}
+
+export default Test
