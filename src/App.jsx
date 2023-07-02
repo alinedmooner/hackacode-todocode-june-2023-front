b7a13b2b48ba54b3f13481187e80b5f7cb7d14cc
Daniel M.C <alinedmooner@gmail.com>
2023-07-02 18:28:33 -0500

    ✨ feat: redux setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Home from './pages/Home'
+import { Outlet } from 'react-router-dom'
 
 function App () {
   return (
-    <div>
-      <Home />
-    </div>
+    <>
+      <Outlet />
+    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import App from './App'
+import Test from './pages/Test'
+import Test2 from './pages/Test2'
+import Home from './pages/Home'
+import './styles/global.css'
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={<App />}>
+    <Route path="/" index={true} element={<Home />} />
+    <Route path="/test/" element={<Test />} />
+    <Route path="/test2/" element={<Test2 />} />
+  </Route>
+))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
