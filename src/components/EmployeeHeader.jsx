1adda3f1d501a50f7cbe0331666ad561ae4ff81e
Daniel M.C <alinedmooner@gmail.com>
2023-07-02 18:27:19 -0500

    ✨ feat: header component

---
+++ src/components/EmployeeHeader.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function EmployeeHeader () {
+  return (
+    <header>
+      <div>
+        EN
+      </div>
+      <div>
+        Dark
+      </div>
+      <div>
+        Employee name
+      </div>
+      <div>
+        <img src="" alt="Employee profile" />
+      </div>
+    </header>
+  )
+}
+
+export default EmployeeHeader
