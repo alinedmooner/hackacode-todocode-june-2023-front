1b4cf71d5630f93b12a5ada36d706d6a12cb291e
Daniel M.C <alinedmooner@gmail.com>
2023-07-03 19:24:30 -0500

    ✨ feat: test

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -3,9 +3,14 @@ import EmployeeNavigation from '../components/EmployeeNavigation'
 import EmployeeHeader from '../components/EmployeeHeader'
 
 function Test () {
-  return <div>
-    <EmployeeHeader />
+  return <div className='employee-container'>
     <EmployeeNavigation />
+    <section className="employee-dashboard">
+      <EmployeeHeader />
+      <main className='employee-test'>
+        <h1>Test</h1>
+      </main>
+    </section>
   </div>
 }
 
