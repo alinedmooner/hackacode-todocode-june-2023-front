670a1aeef0b7e61b483c08e581bb60de303abcb5
Daniel M.C <alinedmooner@gmail.com>
2023-07-03 19:24:18 -0500

    ✨ feat: navigation

--- src/components/EmployeeNavigation.jsx
+++ src/components/EmployeeNavigation.jsx
@@ -2,30 +2,18 @@ import React from 'react'
 
 function EmployeeNavigation () {
   return (
-    <nav>
-      <h1>Employee Status</h1>
-      <ul>
-        <li>
-          <a href="/employee">Home</a>
-        </li>
-        <li>
-          <a href="/employee/attractions">Attractions</a>
-        </li>
-        <li>
-          <a href="/employee/categories">Categories</a>
-        </li>
-        <li>
-          <a href="/employee/employees">Employees</a>
-        </li>
-        <li>
-          <a href="/employee/visitors">Visitors</a>
-        </li>
-        <li>
-          <a href="/employee/visits">Visits</a>
-        </li>
-      </ul>
-      <div>
-        <a href="/employee/logout">Logout</a>
+    <nav className='employee-navigation'>
+      <h1 className='dashboard-title main-text'>Employee Status</h1>
+      <div className='employee-list'>
+          <div className='employee-list-group'>
+            <a className='employee-list-element' href="/employee">Home</a>
+            <a className='employee-list-element' href="/employee/attractions">Attractions</a>
+            <a className='employee-list-element' href="/employee/categories">Categories</a>
+            <a className='employee-list-element' href="/employee/employees">Employees</a>
+            <a className='employee-list-element' href="/employee/visitors">Visitors</a>
+            <a className='employee-list-element' href="/employee/visits">Visits</a>
+          </div>
+          <a className='employee-list-element employee-logout' href="/employee/logout">Logout</a>
       </div>
     </nav>
   )
